42c9ac255316223766740d7cb39cc2b19b1fec7d
AdamChrimes <iChrimes@live.co.uk>
2019-04-01 16:14:34 +0100

    use google analytics instead of adobe analytics
    Adobe Analytics requires jQuery to function properly, we want
    to move away from using jQuery if possible. So Google analytics
    has been chosen as our analytics tool moving forward.

--- app/locals.js
+++ app/locals.js
@@ -1,16 +1,8 @@
 module.exports = config => (req, res, next) => {
-  res.locals.serviceName = config.serviceName;
-  res.locals.env = config.env;
-
-  if (config.env === 'development') {
-    res.locals.ADOBE_TRACKING_URL = config.analytics.development;
-  } else if (config.env === 'staging') {
-    res.locals.ADOBE_TRACKING_URL = config.analytics.staging;
-  } else {
-    res.locals.ADOBE_TRACKING_URL = config.analytics.production;
-  }
-
   res.locals.COOKIE_CONSENT_URL = config.cookies.scriptURL;
+  res.locals.env = config.env;
+  res.locals.GOOGLE_UA = config.analytics.googleUA;
+  res.locals.serviceName = config.serviceName;
 
   next();
 };
