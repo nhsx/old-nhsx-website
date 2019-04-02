5944d76e6e3703273538942ef06e0d0ab63e4a87
AdamChrimes <iChrimes@live.co.uk>
2019-04-02 09:42:15 +0100

    remove unused local variables

--- app/locals.js
+++ app/locals.js
@@ -1,8 +1,6 @@
 module.exports = config => (req, res, next) => {
   res.locals.COOKIE_CONSENT_URL = config.cookies.scriptURL;
-  res.locals.env = config.env;
   res.locals.GOOGLE_UA = config.analytics.googleUA;
-  res.locals.serviceName = config.serviceName;
 
   next();
 };
